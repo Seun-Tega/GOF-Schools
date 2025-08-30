4c2e1a65a640be7fc284e2e32500d0ad74d29262
swiftly01 <kareemkazeem100@gmail.com>
2025-08-30 11:52:13 +0100

    fix(resolve): resolve all changes

--- resources/views/pages/application.blade.php
+++ resources/views/pages/application.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="icon" href="{{ asset('assets/image/image (6).png') }}">
-  <link rel="stylesheet" href="{{ asset('assets/gof-css/admission.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/gof-css/admission.css') }}?{{ time() }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
