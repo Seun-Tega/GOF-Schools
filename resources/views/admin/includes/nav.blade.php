338d46e4111eb186b277e19d8013faee2e6ee8ae
swiftly01 <kareemkazeem100@gmail.com>
2025-09-02 21:57:14 +0100

    fix(footer): footer year fixed

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -181,11 +181,11 @@
               </form>
             </div>
           </li>
-          <li class="nav-item nav-settings d-none d-lg-block">
+          {{-- <li class="nav-item nav-settings d-none d-lg-block">
             <a class="nav-link" href="#">
               <i class="fas fa-ellipsis-h"></i>
             </a>
-          </li>
+          </li> --}}
         </ul>
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
           <span class="fas fa-bars"></span>
